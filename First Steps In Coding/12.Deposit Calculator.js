c6eb2bde61f8c3f805e2dfc0abe324dd17b8b555
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2023-04-10 05:48:15 +0100

    Homework Exercise

--- First Steps In Coding/12.Deposit Calculator.js
+++ First Steps In Coding/12.Deposit Calculator.js
@@ -3,7 +3,6 @@ let depositedSum = Number(input[0])
 let timeOfDeposit = Number(input[1])
 let annualInterestPercent = Number((input[2])) / 100
 
-
 let sum = depositedSum + timeOfDeposit * ((depositedSum * annualInterestPercent) / 12)
 console.log(sum)
 }
