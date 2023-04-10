660fe7d454c3dcacfef0109448133f8631e973c1
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2023-04-10 06:07:54 +0100

    Homework Exercise

--- First Steps In Coding/14.Supplies for School.js
+++ First Steps In Coding/14.Supplies for School.js
@@ -1,10 +1,10 @@
  function suppliesForSchool(input) {
- let packOfPens = Number(input[0])
- let packOfMarkers = Number(input[1])
+ let packsOfPens = Number(input[0])
+ let packsOfMarkers = Number(input[1])
  let chemickal = Number(input[2])
  let percentDiscount = Number(input[3]) / 100
 
- let totalSum = (packOfPens * 5.80) + (packOfMarkers * 7.20) + (chemickal * 1.20)
+ let totalSum = (packsOfPens * 5.80) + (packsOfMarkers * 7.20) + (chemickal * 1.20)
  let discount = totalSum * percentDiscount
 
  let finalPrice = totalSum - discount
