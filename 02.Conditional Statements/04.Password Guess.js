d6fa244a9bd315d683163a0f344b4d275a584fe2
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2023-04-11 09:09:21 +0100

    Homework Lab

---
+++ 02.Conditional Statements/04.Password Guess.js
@@ -0,0 +1,9 @@
+function passwordGuess(input) {
+let password = "s3cr3t!P@ssw0rd"
+let currentPassword = input[0]
+
+if (password === currentPassword) 
+console.log('Welcome')
+
+else (console.log('Wrong password!'))
+}
