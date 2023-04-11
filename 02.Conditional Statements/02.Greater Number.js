d8e15966f61355b94ec954279fef4d3e854cafc0
Hristian Balevski <114162692+HristianBalevski@users.noreply.github.com>
2023-04-11 09:07:58 +0100

    Homework Lab

--- 02.Conditional Statements/02.Greater Number.js
+++ 02.Conditional Statements/02.Greater Number.js
@@ -1,9 +1,10 @@
 function greaterNumber(input) {
-let firstNumber = Number(input[0])
-let secondNumber = Number(input[1])
-
-if (firstNumber > secondNumber)
-console.log(firstNumber)
-
-else if(console.log(secondNumber));
-}
+    let firstNumber = Number(input[0])
+    let secondNumber = Number(input[1])
+    
+    if (firstNumber > secondNumber)
+    console.log(firstNumber)
+    
+    else 
+    console.log(secondNumber)
+    }
